59491621f115245ab690d4d8662f4a99ac67bc74
Yang Luo <yangluo1105@gmail.com>
2023-01-31 23:40:58 +1100

    First commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+
 
 export default createStore({
   state: {
+    products: [],
   },
   mutations: {
+
+    loadProducts(state, products){
+      console.log(products);
+      state.products = products;
+
+    }
+
+    
   },
   actions: {
+
+    loadProducts({ commit }) {
+      axios
+      .get('https://fakestoreapi.com/products')
+      .then(
+        response => {
+          commit('loadProducts', response.data)
+        }
+
+      )
+    }
+
   },
   modules: {
   }
